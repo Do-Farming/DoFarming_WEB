91ac7a2d40263a2a5d7c23f9964b9cff19f6e836
이윤아 <101340482+Profitah@users.noreply.github.com>
2024-02-02 05:46:43 +0900

    Map import edit

--- src/Pages/Map/Map.jsx
+++ src/Pages/Map/Map.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { GoogleMap, LoadScript, Marker, InfoWindow, StandaloneSearchBox } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, MarkerF, InfoWindowF, StandaloneSearchBox } from '@react-google-maps/api';
 import dot from './제목_없는_아트워크.png';
 import NavBar from "../Nav/Nav.jsx";
 import '../../Styles/Map/Map.css'
@@ -114,7 +114,7 @@ const MyComponent = () => {
           onLoad={ref => setMap(ref)}
         >
           {initialLocation && (
-            <Marker
+            <MarkerF
               position={initialLocation}
               icon={{ 
                 url: dot,
@@ -123,7 +123,7 @@ const MyComponent = () => {
             />
           )}
           {places.slice(0, 5).map((place, i) => (
-            <Marker
+            <MarkerF
               key={i}
               position={place.geometry.location}
               onClick={() => {
@@ -132,7 +132,7 @@ const MyComponent = () => {
             />
           ))}
           {selectedPlace && (
-            <InfoWindow
+            <InfoWindowF
               position={{ lat: selectedPlace.geometry.location.lat(), lng: selectedPlace.geometry.location.lng() }}
               onCloseClick={() => {
                 setSelectedPlace(null);
@@ -142,7 +142,7 @@ const MyComponent = () => {
                 <h2>{selectedPlace.name}</h2>
                 <p>{selectedPlace.formatted_address}</p>
               </div>
-            </InfoWindow>
+            </InfoWindowF>
           )}
           <StandaloneSearchBox
             onLoad={onLoad}
