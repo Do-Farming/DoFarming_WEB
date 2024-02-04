9a385d74b88e481493dd1b6a2e906eb5cd99ea98
이윤아 <101340482+Profitah@users.noreply.github.com>
2024-02-04 23:50:45 +0900

    Connected routine and package links, and set the navigation icon to link to the home screen when clicked.

--- src/Pages/Home/MakeRoutine.jsx
+++ src/Pages/Home/MakeRoutine.jsx
@@ -1,37 +1,20 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { AiOutlineUser } from "react-icons/ai";
-import { FiAlignJustify } from "react-icons/fi";
 import "../../Styles/Home/Home.css";
+import "../../Pages/Nav/Nav.jsx";
+import NavBar from "../../Pages/Nav/Nav.jsx";
 
-const MakeRoutine = () => {
-  const [isNavVisible, setIsNavVisible] = useState(false);
-
-  const toggleNav = () => {
-    setIsNavVisible(!isNavVisible);
-  };
-
-
+const MakeRoutine = ({ packageName }) => {
 
   return (
     <div className="HomeWrap">
-      <div className="Nav">
-        <AiOutlineUser />
-        <FiAlignJustify onClick={toggleNav} />
-        {isNavVisible && (
-          <ul className="nav-menu">
-            <li>홈</li>
-            <li>루틴</li>
-            <li>전문가와의 상담</li>
-            <li>고민 노크</li>
-          </ul>
-        )}
-      </div>
-    <p>아침루틴</p>
-    <p>아직 트랙이 없습니다</p> 
-    <Link to="/Routine">
-      <p id="goRoutine">루틴을 만들러 이동해 볼까요?</p>
-    </Link>
+      <NavBar/>
+      
+      <p>{packageName}</p>
+      <p>아직 트랙이 없습니다</p> 
+      <Link to="/Routine">
+        <p id="goRoutine">루틴을 만들러 이동해 볼까요?</p>
+      </Link>
     </div>
   );
 };
