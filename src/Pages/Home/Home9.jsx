70e803c4ffbebbfbcb5b081feb8ae3764b2b09a2
이윤아 <101340482+Profitah@users.noreply.github.com>
2024-02-06 17:57:33 +0900

    Home 9 add move

--- src/Pages/Home/Home9.jsx
+++ src/Pages/Home/Home9.jsx
@@ -1,50 +1,69 @@
-import React, { useState, useRef } from 'react';
-import DatePicker from 'react-datepicker';
-import { FaCalendarAlt } from 'react-icons/fa';
-import '../../Styles/Home/Home9.css';
-import TextInput from './TextInput'; 
-import '../../Styles/Home/EditableBox.css';
+        import React, { useState, useRef, forwardRef } from 'react';
+        import DatePicker from 'react-datepicker';
+        import { FaCalendarAlt } from 'react-icons/fa';
+        import '../../Styles/Home/Calendar.css';
+        import TextInput from './TextInput'; 
+        import '../../Styles/Home/Home9.css';
+        import NavBar from '../Nav/Nav.jsx';
 
-function Home9() {
-  const [dateRange, setDateRange] = useState([new Date(), new Date()]);
-  const [startDate, endDate] = dateRange;
-  const [isOpen, setIsOpen] = useState(false);
-  const ref = useRef();
+        function Home9() {
+        const [dateRange, setDateRange] = useState([new Date(), new Date()]);
+        const [startDate, endDate] = dateRange;
+        const [isOpen, setIsOpen] = useState(false);
+        const datePickerRef = useRef();
 
-  const CustomInput = ({ value, onClick }) => (
-    <div>
-      <input 
-        type="text" 
-        value={value} 
-        style={{ width: '82%' }}
-        onChange={e => setDateRange(e.target.value.split(' - ').map(date => new Date(date)))}
-      />
-      <FaCalendarAlt onClick={onClick} style={{cursor: 'pointer'}} />
-    </div>
-  );
+        const CustomInput = forwardRef(({ value, onClick }, ref) => (
+            <div>
+            <input 
+                ref={ref}
+                type="text" 
+                value={value} 
+                style={{ width: '82%' }}
+                onChange={(e) => setDateRange(e.target.value.split(' - ').map(date => new Date(date)))}
+            />
+            <FaCalendarAlt onClick={onClick} style={{ cursor: 'pointer' }} />
+            </div>
+        ));
 
-  return (
-    <div>
-      <TextInput className=''init="아침루틴" /> 
-      <TextInput init="끄적끄적" /> 
-      <h2>시작일자와 종료일자</h2>
-      <DatePicker
-        ref={ref}
-        selected={startDate}
-        onChange={(update) => {
-          setDateRange(update);
-        }}
-        startDate={startDate}
-        endDate={endDate}
-        selectsRange
-        customInput={<CustomInput />}
-        dateFormat="MM/dd/yy"
-        open={isOpen}
-        onCalendarClose={() => setIsOpen(false)}
-        onCalendarOpen={() => setIsOpen(true)}
-      />
-    </div>
-  );
-}
+        return (
+            <div>
+            <div className='Home9Wrap'>
+            <NavBar/>
+            < div id='RouName'>
+            <TextInput init="아침루틴" /> 
+            </div>
+            <div id='UserPK'>
+            <div id='SangMe'>
+            <TextInput init="끄적끄적" /> 
+            </div>
 
-export default Home9;
+            <div id='DayDay'>
+            <p>시작일자와 종료일자</p>
+            <DatePicker
+                ref={datePickerRef}
+                selected={startDate}
+                onChange={(update) => {
+                setDateRange(update);
+                }}
+                startDate={startDate}
+                endDate={endDate}
+                selectsRange
+                customInput={<CustomInput />}
+                dateFormat="MM/dd/yy"
+                open={isOpen}
+                onCalendarClose={() => setIsOpen(false)}
+                onCalendarOpen={() => setIsOpen(true)}
+            />
+                </div>
+            </div>
+            <div className='BtnWrap'>
+            <Link to="/Home">
+            <button>완료</button>
+        </Link>        
+        </div>
+            </div>
+            </div>
+        );
+        }
+
+        export default Home9;
