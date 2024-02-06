042d330db886e2f38a0c630438c84ca0a5094f50
이윤아 <101340482+Profitah@users.noreply.github.com>
2024-02-07 03:35:23 +0900

    Change deletion method

--- src/Pages/Home/PackageDelete.jsx
+++ src/Pages/Home/PackageDelete.jsx
@@ -1,17 +1,20 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { RiPencilFill } from 'react-icons/ri';
+import '../../Styles/Home/PackageDelete.css';
 
 const PackageDelete = ({ packageInfo, handleDeletePackage }) => {
   const { id, name, status } = packageInfo;
   const navigate = useNavigate();
+  const [showDeleteBtn, setShowDeleteBtn] = useState(false);
   const [showModal, setShowModal] = useState(false);
 
   const handleClick = () => {
-    setShowModal(true);
+    setShowDeleteBtn(true);
   };
 
   const handleConfirmDelete = () => {
-    handleDeletePackage(id);
+    handleDeletePackage(id); 
     setShowModal(false);
   };
 
@@ -19,22 +22,26 @@ const PackageDelete = ({ packageInfo, handleDeletePackage }) => {
     setShowModal(false);
   };
 
-  const isEdit = () => {
-    navigate("/MakeRoutine");
-  };
-
   return (
     <div>
       <h3>{name}</h3>
       <p>{status}</p>
-      <button onClick={handleClick}>Delete</button>
-      <button onClick={isEdit}>Edit</button>
-
-      {showModal && (
+      <button onClick={() => navigate('/Home2_1_1')}>
+              <RiPencilFill /> Edit
+            </button>
+      {!showDeleteBtn && <button onClick={handleClick}>...</button>}
+      {showDeleteBtn && (
         <div>
-          <p>삭제하시겠습니까?</p>
-          <button onClick={handleConfirmDelete}>확인</button>
-          <button onClick={handleCancelDelete}>취소</button>
+          <div>
+            <button onClick={() => setShowModal(true)}>Delete</button>
+          </div>
+          {showModal && (
+            <div>
+              <p>삭제하시겠습니까?</p>
+              <button onClick={handleConfirmDelete}>확인</button>
+              <button onClick={handleCancelDelete}>취소</button>
+            </div>
+          )}
         </div>
       )}
     </div>
