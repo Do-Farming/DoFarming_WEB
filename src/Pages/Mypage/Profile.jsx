b56bcb27417f68892768648dc6b4c1f5f74f4647
이윤아 <101340482+Profitah@users.noreply.github.com>
2024-02-07 11:29:55 +0900

    My Page Nickname Validation Check

--- src/Pages/Mypage/Profile.jsx
+++ src/Pages/Mypage/Profile.jsx
@@ -3,6 +3,26 @@ import "../../Styles/Home/Home.css";
 import NavBar from "../Nav/Nav.jsx";
 import myimg from "./기본이미지.png";
 
+const eng = /[a-zA-Z]/;
+const kor = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
+const num = /[0-9]/;
+const spc = /[~!@#$%^&*()_+|<>?:{}]/;
+
+const NicknameCheck = (e) => {
+  const input = e.target.value;
+  if (
+    input.length >= 0 &&
+    input.length <= 12 &&
+    (eng.test(input) || kor.test(input) || num.test(input) || !spc.test(input))
+  ) {
+    setNickname(input);
+  } else {
+    alert(
+      "닉네임은 영문, 한글, 숫자를 포함한 12글자 이하여야 하며 특수기호를 포함하지 않아야 합니다."
+    );
+  }
+};
+
 const Profile = () => {
   // 상태 관리
   const [nickname, setNickname] = useState("닉네임"); // 서버에서 사용자 닉네임 가져오기
@@ -14,7 +34,6 @@ const Profile = () => {
 
   // input 요소에 대한 참조
   const fileInputRef = useRef(null);
-
 
   // 파일 입력 변경 핸들러
   const handleImageChange = (e) => {
@@ -40,8 +59,21 @@ const Profile = () => {
 
   // 닉네임 저장 핸들러
   const handleSaveNickname = () => {
-    setNickname(newNickname);
-    setEditingNickname(false);
+    if (
+      newNickname.length >= 0 &&
+      newNickname.length <= 12 &&
+      (eng.test(newNickname) ||
+        kor.test(newNickname) ||
+        num.test(newNickname) ||
+        !spc.test(newNickname))
+    ) {
+      setNickname(newNickname);
+      setEditingNickname(false);
+    } else {
+      alert(
+        "닉네임은 영문, 한글, 숫자를 포함한 12글자 이하여야 하며 특수기호를 포함하지 않아야 합니다."
+      );
+    }
   };
 
   // 성별 변경 핸들러
@@ -58,20 +90,30 @@ const Profile = () => {
     <div className="ProfileWrap">
       <NavBar />
       <div>
-          <div>Profile</div>
-      <div>
+        <div>Profile</div>
+        <div>
           {/* 이미지를 표시할 곳 */}
           {image ? (
-            <img onClick={handleCustomButtonClick} src={image} alt="Uploaded" style={{ width: "200px", height: "200px" }} />
+            <img
+              onClick={handleCustomButtonClick}
+              src={image}
+              alt="Uploaded"
+              style={{ width: "200px", height: "200px" }}
+            />
           ) : (
-            <img onClick={handleCustomButtonClick} src={myimg} alt="Default" style={{ width: "200px", height: "200px" }} />
+            <img
+              onClick={handleCustomButtonClick}
+              src={myimg}
+              alt="Default"
+              style={{ width: "200px", height: "200px" }}
+            />
           )}
         </div>
         <div>
           {/* 숨겨진 파일 입력 */}
           <input
             type="file"
-            accept="image/*"
+            accept="image/"
             onChange={handleImageChange}
             ref={fileInputRef}
             style={{ display: "none" }}
@@ -80,7 +122,11 @@ const Profile = () => {
         <div>
           {editingNickname ? (
             <div>
-              <input type="text" value={newNickname} onChange={handleNicknameChange} />
+              <input
+                type="text"
+                value={newNickname}
+                onChange={handleNicknameChange}
+              />
               <button onClick={handleSaveNickname}>저장</button>
             </div>
           ) : (
