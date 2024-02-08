450b46324474cab5963aa8614fc81e4d9b1314a5
yeeun park <145003970+yengniws@users.noreply.github.com>
2024-02-08 17:57:10 +0900

    FEAT:Nav 고민노크 삭제

--- src/Pages/Nav/Nav.jsx
+++ src/Pages/Nav/Nav.jsx
@@ -32,7 +32,7 @@ const NavBar = () => {
                     <Link to="/home"><li>홈</li></Link>
                     <Link to="/routine"><li>루틴</li></Link>
                     <Link to="/Map"><li>전문가 찾기</li></Link>
-                    <Link to="/"><li>고민 노크</li></Link>
+                    {/* <Link to="/"><li>고민 노크 (삭제)</li></Link> */}
                 </ul>
             )}
         </div>
