84fe6e06b78501c4965efb8129960eaf2dc069fd
yeeun park <145003970+yengniws@users.noreply.github.com>
2024-02-08 21:19:35 +0900

    FEAT:Home 9 edit, STYLE:Home9 complete

--- src/Pages/Home/Home9.jsx
+++ src/Pages/Home/Home9.jsx
@@ -2,73 +2,89 @@ import React, { useState, useRef, forwardRef } from 'react';
 import DatePicker from 'react-datepicker';
 import { FaCalendarAlt } from 'react-icons/fa';
 import '../../Style/Home/Calendar.css';
-import TextInput from './TextInput'; 
 import '../../Style/Home/Home9.css';
 import NavBar from '../Nav/Nav.jsx';
-
+import { Link } from 'react-router-dom';
 
 function Home9() {
-const [dateRange, setDateRange] = useState([new Date(), new Date()]);
-const [startDate, endDate] = dateRange;
-const [isOpen, setIsOpen] = useState(false);
-const datePickerRef = useRef();
+  const [dateRange, setDateRange] = useState([new Date(), new Date()]);
+  const [startDate, endDate] = dateRange;
+  const [isOpen, setIsOpen] = useState(false);
+  const datePickerRef = useRef();
+  const [routine, setRoutine] = useState();
+  const [memo, setMemo] = useState();
 
-const handleButtonClick = () => {
+  const handleButtonClick = () => {
     alert("저장되었습니다!");
-};
+  };
 
-const CustomInput = forwardRef(({ value, onClick }, ref) => (
+  const CustomInput = forwardRef(({ value, onClick }, ref) => (
     <div>
-    <input 
+      <input 
         ref={ref}
         type="text" 
         value={value} 
-        style={{ width: '82%' }}
+        readOnly
+        className="Home9inputboxDate"
         onChange={(e) => setDateRange(e.target.value.split(' - ').map(date => new Date(date)))}
-    />
-    <FaCalendarAlt onClick={onClick} style={{ cursor: 'pointer' }} />
+      />
+      <FaCalendarAlt onClick={onClick} className='Home9inputboxDateIcon'/>
     </div>
-));
+  ));
 
-return (
+  return (
     <div>
-        <NavBar/>
-        <div className='Home9Wrap'>
-            
-            < div id='RouName'>
-                <TextInput init="아침루틴" /> 
-            </div>
-            <div id='UserPK'>
-            <div id='SangMe'>
-                메모
-                <TextInput init="글자 수 제한"/> 
-            </div>
-
-        <div id='DayDay'>
-        <div>기간</div>
-        <DatePicker
-            ref={datePickerRef}
-            selected={startDate}
-            onChange={(update) => {
-            setDateRange(update);
-            }}
-            startDate={startDate}
-            endDate={endDate}
-            selectsRange
-            customInput={<CustomInput />}
-            dateFormat="yy/MM/dd"
-            open={isOpen}
-            onCalendarClose={() => setIsOpen(false)}
-            onCalendarOpen={() => setIsOpen(true)}
-        />
-            </div>
+      <NavBar/>
+      <div className='Home9Wrap'>
+        <div className='PackageName'>
+          <input 
+            type="text"
+            value={routine}
+            onChange={(e) => setRoutine(e.target.value)}
+            onBlur={() => setRoutine(routine)}
+            placeholder='루틴 이름'
+            className='Home9inputboxname'
+          />
+        </div>
+        <div className='Home9inputWrap'>
+          <div className='Home9input'>
+            <div>메모</div>
+            <input 
+              type="text"
+              value={memo}
+              onChange={(e) => setMemo(e.target.value)}
+              onBlur={() => setMemo(memo)}
+              placeholder='글자 수 제한'
+              className='Home9inputbox'
+            />
+          </div>
+          <div className='Home9input'>
+            <div>기간</div>
+            <DatePicker
+              ref={datePickerRef}
+              selected={startDate}
+              onChange={(update) => {
+                setDateRange(update);
+              }}
+              startDate={startDate}
+              endDate={endDate}
+              selectsRange
+              customInput={<CustomInput />}
+              dateFormat="yy/MM/dd"
+              open={isOpen}
+              onCalendarClose={() => setIsOpen(false)}
+              onCalendarOpen={() => setIsOpen(true)}
+            />
+          </div>
         </div>
         <div className='BtnWrap'>
-            <button className='Home9Btn' onClick={handleButtonClick}>완료</button>      
+            <Link to="/home">
+                <button className='Home9Btn' onClick={handleButtonClick}>완료</button>  
+            </Link>    
         </div>
-        </div>
+      </div>
     </div>
-);
+  );
 }
 
 export default Home9;
