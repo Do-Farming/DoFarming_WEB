0f880afb96fc3d827ab70331e5bcc9a0e6304c69
yeeun park <145003970+yengniws@users.noreply.github.com>
2024-02-11 21:38:45 +0900

    login1 styled-components

--- src/main.jsx
+++ src/main.jsx
@@ -13,9 +13,6 @@ import Reset from "./Pages/Mypage/Reset";
 import HomeAddPackage from "./Pages/Home/HomeAddPackage";
 import Todo from "./Pages/Home/Todo";
 
-import "./Style/Home/Nav.css";
-import "./Style/Login/Login1.css";
-
 const root = document.getElementById("root");
 const rootElement = createRoot(root);
 
