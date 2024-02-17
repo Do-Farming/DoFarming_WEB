ba30d7355d79838c239cfaf83e1bd00a5b7e3b63
yeeun park <145003970+yengniws@users.noreply.github.com>
2024-02-18 01:42:14 +0900

    3

--- src/Pages/Login/Login3.jsx
+++ src/Pages/Login/Login3.jsx
@@ -8,7 +8,7 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
-  height: 91vh;
+  height: 90vh;
   overflow: hidden;
   @media screen and (min-width: 1280px) {
     height: 100vh;
