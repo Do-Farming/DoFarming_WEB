0661dee56cb21450193cbc8d0ae570e68bd6ed4c
I, yoonah <qwer120542@gmail.com>
2024-03-10 01:10:41 +0900

    Test : vercel 배포 오류 수정

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,6 @@ import HomeAddPackage from "./Pages/Home/HomeAddPackage";
 import Todo from "./Pages/Home/Todo";
 import HomeSection2 from "./Pages/Home/HomeComponents/HomeSection2";
 
-
 const root = document.getElementById("root");
 const rootElement = createRoot(root);
 
@@ -35,7 +34,6 @@ rootElement.render(
       <Route path="/HomeAddPackage/" element={<HomeAddPackage />} />
       <Route path="/Todo/" element={<Todo />} />
       <Route path="/HomeSection2/" element={<HomeSection2 />} />
-      <Route path=""
     </Routes>
   </Router>
 );
